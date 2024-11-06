b340c56305bd390f0d743e63bcacc2a5b37172ca
Elvin cooper <ing.elvin01cooper@gmail.com>
2024-11-06 15:49:14 -0400

    CRUD completo de la API

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///contacts.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///datos.db"
 db = SQLAlchemy(app)
 
 # Creando modelo de base de datos
@@ -20,8 +20,60 @@ class Contacto(db.Model):
         'phone':self.phone
         }
     
-    # Creando las tablas en la base de datos
-    with app.app_context():
-        db.create_all() 
+# Creando las tablas en la base de datos
+with app.app_context():
+    db.create_all() 
+
+
+# Creando rutas o endpoints        
+@app.route('/contact', methods=['GET'])
+def get_contacts():
+    contacts = Contacto.query.all()
+    return jsonify({'contatcs': [contact.serializar() for contact in contacts]})
+    db.session.close()
+
+@app.route('/insertar', methods=['POST'])
+def create_contact():
+    data = request.get_json()    
+    mi_contacto = Contacto(name=data['name'], email=data['email'], phone=data['phone'])
+    db.session.add(mi_contacto)
+    db.session.commit()
+    return jsonify({'mensaje':'El contacto ha sido creado con exito', 'contacto': mi_contacto.serializar()}), 201
+    
+
+@app.route('/contact/<int:id>', methods=['GET'])
+def get_contact(id):
+    contacto = Contacto.query.get(id)
+    if not contacto:
+        return jsonify({'mensaje':'el contacto no existe'}), 404
+    return jsonify({'mensaje':'El contacto ha sido creado con exito',
+                    'contacto': contacto.serializar()}), 201
+
+
+@app.route('/contact/<int:id>', methods=['PUT', 'PATCH'])
+def update_contact(id):
+    contacto = Contacto.query.get_or_404(id)
+    data = request.get_json()
+
+    if 'name' in data:
+        contacto.name = data['name']
+    if 'email' in data:
+        contacto.email = data['email']
+    if 'phone' in data:
+        contacto.phone = data['phone']
+
+    # guardando los cambios en la base de datos
+    db.session.commit()
+    return jsonify({'mensaje':'El contacto ha sido actualizado con exito','contacto': contacto.serializar()}), 201
+
+
+@app.route('/contact/<int:id>', methods=['DELETE'])
+def delete_contact(id):
+    contacto =  Contacto.query.get(id)
+    if contacto:
+        db.session.delete(contacto)
+        db.session.commit()
+        return jsonify({'mensaje':'Contacto eliminado con exito','contacto': contacto.serializar()}), 201
+    return jsonify({'mensaje':'El contacto no existe'}), 404
 
 
